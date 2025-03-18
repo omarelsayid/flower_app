ae0996c7785204207bef61e571bd8f5e85d06ecb
omarelsayed <omarelsayd445@gmail.com>
2025-03-19 01:26:11 +0200

    add constants and end points files

---
+++ lib/core/end_points.dart
@@ -0,0 +1,1 @@
+const String baseUrl = 'https://exam.elevateegy.com/api/v1/';
---
+++ lib/core/utils/constans.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+double kHorizontalPadding(BuildContext context) {
+  return MediaQuery.of(context).size.width * 0.04; //! 4% of screen width ==(16)
+}
