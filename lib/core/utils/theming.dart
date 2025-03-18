afdfc2eddc95c8d69c19303222a3c5a1dbed8200
omarelsayed <omarelsayd445@gmail.com>
2025-03-19 01:57:54 +0200

    add app colors files and start working on theming file

--- lib/core/utils/theming.dart
+++ lib/core/utils/theming.dart
@@ -1,4 +1,24 @@
-import 'package:flower_app/core/utils/text_styles.dart';
+import 'package:flower_app/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 
+ThemeData theme(BuildContext context) {
+  final double screenWidth = MediaQuery.of(context).size.width;
+  final double screenHeight = MediaQuery.of(context).size.height;
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: primaryColor,
+        minimumSize: Size(
+          screenWidth * (screenWidth / 343),
+          screenHeight * (screenHeight / 48),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(screenWidth * 0.1),
+        ),
+      ),
+    ),
+  );
 
+ 
+}
