567d21bfaa254817916e7ac9e32347fd589f522c
omarelsayed <omarelsayd445@gmail.com>
2025-03-19 02:22:22 +0200

    fix bug in elevated button widht and height in theming file

--- lib/core/utils/theming.dart
+++ lib/core/utils/theming.dart
@@ -1,4 +1,5 @@
 import 'package:flower_app/core/utils/app_colors.dart';
+import 'package:flower_app/main.dart';
 import 'package:flutter/material.dart';
 
 ThemeData theme(BuildContext context) {
@@ -9,16 +10,26 @@ ThemeData theme(BuildContext context) {
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: primaryColor,
-        minimumSize: Size(
-          screenWidth * (screenWidth / 343),
-          screenHeight * (screenHeight / 48),
-        ),
+        minimumSize: Size((343 / 375) * screenWidth, (48 / 812) * screenHeight),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(screenWidth * 0.1),
         ),
       ),
     ),
   );
+}
 
- 
+OutlineInputBorder textFiledInputBorder() {
+  final screenWidth = navigatorKey.currentState!.context.size!.width;
+
+  double responsiveRadius = (4 / 375.0) * screenWidth;
+  double responsiveBorderWidth = (1 / 375.0) * screenWidth;
+
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(responsiveRadius),
+      topRight: Radius.circular(responsiveRadius),
+    ),
+    borderSide: BorderSide(color: Colors.grey, width: responsiveBorderWidth),
+  );
 }
