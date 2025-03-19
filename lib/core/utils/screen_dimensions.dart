70a56594773998a8b16c1c13c7243b1126a99e63
omarelsayed <omarelsayd445@gmail.com>
2025-03-19 03:10:17 +0200

    add base width and height

--- lib/core/utils/screen_dimensions.dart
+++ lib/core/utils/screen_dimensions.dart
@@ -2,3 +2,5 @@ import 'package:flower_app/main.dart';
 
 final screenWidth = navigatorKey.currentState!.context.size!.width;
 final screenHeight = navigatorKey.currentState!.context.size!.height;
+const baseWidth = 375.0;
+const baseHeight = 812.0;
