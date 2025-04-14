51604e55968fdbf3d3d97a78f6078f6bfe22fc7e
ALHassan Musleh <80463251+ALHassanMusleh@users.noreply.github.com>
2025-04-14 03:13:21 +0200

    feat(upload-photo):  - add upload photo view model and states.

---
+++ lib/features/profile/main_profile_screen/presentation/cubit/upload_photo_cubit/upload_photo_view_model.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:flower_app/core/common/result.dart';
+import 'package:flower_app/features/profile/main_profile_screen/domain/use_case/upload_photo_use_case.dart';
+import 'package:flower_app/features/profile/main_profile_screen/presentation/cubit/upload_photo_cubit/upload_photo_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class UploadPhotoViewModel extends Cubit<UploadPhotoState> {
+  final UploadPhotoUseCase _uploadPhotoUseCase;
+  UploadPhotoViewModel(this._uploadPhotoUseCase)
+    : super(UploadPhotoInitialState());
+  void uploadPhoto(File photo, String description) async {
+    emit(UploadPhotoLoadingState());
+    final result = await _uploadPhotoUseCase.execute(photo, description);
+    switch (result) {
+      case Success():
+        emit(UploadPhotoSuccessState());
+      case Error():
+        emit(UploadPhotoErrorState(result.exception!));
+    }
+  }
+}
