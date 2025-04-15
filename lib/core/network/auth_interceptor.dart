869b60432814d5c68b052d774e655486297b1a85
Feto505 <177836466+Feto505@users.noreply.github.com>
2025-04-15 14:06:39 +0300

    Fix: endpoints in AuthInterceptor -add endpoints that do not need token to AuthInterceptor.

--- lib/core/network/auth_interceptor.dart
+++ lib/core/network/auth_interceptor.dart
@@ -12,3 +12,26 @@ class AuthInterceptor extends Interceptor {
     return handler.next(options);
   }
 }
+// class AuthInterceptor extends Interceptor {
+//   @override
+//   void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+//     // end points we do not add token.
+//     final excludedPaths = [
+//       "/api/v1/auth/login",
+//       "/api/v1/auth/register",
+//       "/api/v1/auth/verifyResetCode",
+//       "/api/v1/auth/resetPassword",
+//     ];
+//
+//     final needsAuth = !excludedPaths.contains(options.path);
+//
+//     if (needsAuth) {
+//       final token = SharedPreferenceServices.getData(AppConstants.token);
+//       if (token != null) {
+//         options.headers["Authorization"] = "Bearer $token";
+//       }
+//     }
+//
+//     return handler.next(options);
+//   }
+// }
