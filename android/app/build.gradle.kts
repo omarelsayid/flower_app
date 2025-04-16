a775f2aa39333c664cfb4ee0ce991a4ac826c65f
omarelsayed <omarelsayd445@gmail.com>
2025-04-16 12:34:01 +0200

    configration for geocoding

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.flower_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 33
     ndkVersion = "27.0.12077973"
 
     compileOptions {
