053a269f484918f0e6fc65dcd040d0950a3a3e1a
ALHassan Musleh <80463251+ALHassanMusleh@users.noreply.github.com>
2025-04-27 05:19:28 +0300

    feat(orders):   - finished build view for active and completed orders.   - added fun to calculator total for order.   - make reorder and added in cart all order.

--- lib/features/orders/presentation/views/orders_view.dart
+++ lib/features/orders/presentation/views/orders_view.dart
@@ -1,17 +1,27 @@
+import 'package:flower_app/core/di/injectable_initializer.dart';
 import 'package:flower_app/core/utils/text_styles.dart';
+import 'package:flower_app/features/orders/presentation/cubits/get_orders_cubit/get_orders_view_model.dart';
 import 'package:flower_app/features/orders/presentation/views/orders_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OrdersView extends StatelessWidget {
-  const OrdersView({super.key});
+  OrdersView({super.key});
+
+  GetOrdersViewModel getOrdersViewModel = getIt.get<GetOrdersViewModel>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Orders', style: AppTextStyles.roboto500_18),
+    return BlocProvider(
+      create: (context) => getOrdersViewModel,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('My Orders', style: AppTextStyles.roboto500_18),
+        ),
+        body: OrdersViewBody(
+            getOrdersViewModel:getOrdersViewModel
+        ),
       ),
-      body: OrdersViewBody(),
     );
   }
 }
