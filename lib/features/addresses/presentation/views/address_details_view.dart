e5969810954880852542141e399ace5977a74999
Feto505 <177836466+Feto505@users.noreply.github.com>
2025-04-28 00:41:54 +0300

    feat(edit address): -fix bug of not sending street in edit page. -use entity of Address instead of DTO in view of address details.

--- lib/features/addresses/presentation/views/address_details_view.dart
+++ lib/features/addresses/presentation/views/address_details_view.dart
@@ -11,19 +11,29 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../domain/entity/user_addresses_entity.dart';
 
+import 'package:flower_app/core/di/injectable_initializer.dart';
+import 'package:flower_app/core/widgets/custom_app_bar.dart';
+import 'package:flower_app/features/addresses/presentation/cubit/address_details_cubit.dart';
+import 'package:flower_app/features/addresses/presentation/cubit/get_addresses_suggestio_cubit/get_addresses_suggestio_cubit.dart';
+import 'package:flower_app/features/addresses/presentation/cubit/place_details_cubit/place_details_cubit.dart';
+import 'package:flower_app/features/addresses/presentation/views/widgets/address_details_view_body.dart';
+import 'package:flower_app/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../domain/entity/user_addresses_entity.dart';
+
 class AddressDetailsView extends StatelessWidget {
   const AddressDetailsView({super.key});
 
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)?.settings.arguments;
-    late final AddressDTO initialDto;
-    if (args is AddressDTO) {
-      initialDto = args;
-    } else if (args is Address) {
-      initialDto = AddressDTO.fromEntity(args);
+    late final Address initialAddress;
+    if (args is Address) {
+      initialAddress = args;
     } else {
-      initialDto = AddressDTO();
+      initialAddress = Address(); // create an empty Address if no argument passed
     }
     debugPrint("==== Received arguments:   $args");
     debugPrint("==== Type:     ${args.runtimeType}");
@@ -31,9 +41,7 @@ class AddressDetailsView extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => getIt.get<AddressDetailsCubit>()),
-        BlocProvider(
-          create: (context) => getIt.get<GetAddressesSuggestioCubit>(),
-        ),
+        BlocProvider(create: (context) => getIt.get<GetAddressesSuggestioCubit>()),
         BlocProvider(create: (context) => getIt.get<PlaceDetailsCubit>()),
       ],
       child: Scaffold(
@@ -42,8 +50,9 @@ class AddressDetailsView extends StatelessWidget {
           isVisible: true,
           context: context,
         ),
-        body: AddressDetailsViewBody(initialAddress: initialDto),
+        body: AddressDetailsViewBody(initialAddress: initialAddress),
       ),
     );
   }
 }
+
